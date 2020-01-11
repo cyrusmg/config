e94a1646e3b23cec5f1b0ba50783f7b60d43b0eb
Jirka Kulovany <jirka@kulovany.net>
2020-01-11 11:25:30 +0100

    Automatically link all .config files

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -7,11 +7,9 @@ mkdir -p ~/.config/i3 ~/.mplayer ~/.mutt
 ln -s $CONFIG/.bashrc ~/.bashrc
 ln -s $CONFIG/.gitconfig ~/.gitconfig
 ln -s $CONFIG/.vimrc ~/.vimrc
-ln -s $CONFIG/.config/git-prompt.sh ~/.config/git-prompt.sh
-ln -s $CONFIG/.config/i3/config ~/.config/i3/config
-ln -s $CONFIG/.config/i3/fehbg.sh ~/.config/i3/fehbg.sh
-ln -s $CONFIG/.config/i3/i3exit.sh ~/.config/i3/i3exit.sh
-ln -s $CONFIG/.config/i3/i3status.conf ~/.config/i3/i3status.conf
 ln -s $CONFIG/.mplayer/input.conf ~/.mplayer/input.conf
+
+find .config -type d -exec mkdir -p ~/{} \;
+find .config -type f -exec ln -s $CONFIG/{} ~/{} \;
 find .bin -type f -exec ln -s $CONFIG/{} ~/.bin/ \;
 cp $CONFIG/.mutt/* ~/.mutt/
